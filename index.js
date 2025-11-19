4a67d91e20d15bfe67f894c111cd0714e51fa74f
Gabriel <geonelson06@gmail.com>
2025-11-19 14:44:57 -0500

    oop

--- index.js
+++ index.js
@@ -5,7 +5,19 @@
  */
 
 class Creature {
-  // write your code here
+  constructor(name, habitat) {
+    this.name = name;
+    this.habitat = habitat;
+  }
+
+  describe() {
+    return `${this.name} lives in the ${this.habitat}.`;
+  }
+
+  sleep() {
+    return `${this.name} falls asleep in the ${this.habitat}. 😴`;
+  }
+
 }
 
 /**
@@ -15,7 +27,15 @@ class Creature {
  */
 
 class Dragon extends Creature {
- //write your code here
+  constructor(name, habitat, firePower) {
+    super(name, habitat);
+    this.firePower = firePower;
+  }
+
+  specialMove() {
+    return `${this.name} breathes fire with ${this.firePower} intensity! 🔥`;
+  }
+
 }
 
 /**
@@ -25,7 +45,15 @@ class Dragon extends Creature {
  */
 
 class Unicorn extends Creature {
-  //write your code here
+  constructor(name, habitat, sparkleLevel) {
+    super(name, habitat);
+    this.sparkleLevel = sparkleLevel;
+  }
+
+  specialMove() {
+    return `${this.name} heals allies with a sparkle level of ${this.sparkleLevel}! ✨`;
+  }
+
 }
 
 // Export the classes for testing
